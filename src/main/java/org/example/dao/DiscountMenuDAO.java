b5c45eeb396d147845bf1f3b22dd4550f1f71dcd
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-03-09 22:25:16 +0100

    Added classes MenuDAO and DiscountMenuDAO.

---
+++ src/main/java/org/example/dao/DiscountMenuDAO.java
@@ -0,0 +1,10 @@
+package org.example.dao;
+
+import org.example.models.DiscountMenu;
+import org.example.models.Menu;
+
+public class DiscountMenuDAO extends AbstractDAO<DiscountMenu>{
+    public DiscountMenuDAO() {
+        super(DiscountMenu.class);
+    }
+}
---
+++ src/main/java/org/example/dao/MenuDAO.java
@@ -0,0 +1,9 @@
+package org.example.dao;
+
+import org.example.models.Menu;
+
+public class MenuDAO extends AbstractDAO<Menu>{
+    public MenuDAO() {
+        super(Menu.class);
+    }
+}
